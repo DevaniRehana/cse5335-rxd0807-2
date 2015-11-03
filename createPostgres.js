5e090d7a86c5ad35ab8df939084321d8b7b52d79
REHANA DEVANI <Rehana@REHANAs-MacBook-Pro.local>
2015-11-02 20:43:50 -0600

    push code

---
+++ createPostgres.js
@@ -0,0 +1,35 @@
+
+/*
+
+#!/usr/bin/env node
+
+var pg = require("pg");
+
+var conString = "pg://msetubdazzaoeh:tFnxTsFV4abVjFzkBrbFCsGpjX@ec2-107-21-221-59.compute-1.amazonaws.com:5432/d446894mce560p";
+
+var client = new pg.Client(conString);
+client.connect();
+
+client.query("CREATE TABLE IF NOT EXISTS emps(id int, first_name varchar(64), last_name varchar(64), company_name varchar(64), address varchar(64), city varchar(64), county varchar(64), state varchar(64), zip varchar(8), phone1 varchar(15), email varchar(64), web citext )");
+
+*/
+
+
+var pg = require('pg');
+
+pg.connect("postgres://msetubdazzaoeh:tFnxTsFV4abVjFzkBrbFCsGpjX@ec2-107-21-221-59.compute-1.amazonaws.com:5432/d446894mce560p", function(err, client) {
+  if (err) throw err;
+  console.log('Connected to postgres! Getting schemas...');
+  console.log('Creating table...\n');
+/*
+  client
+    .query('SELECT table_schema,table_name FROM information_schema.tables;')
+    .on('row', function(row) {
+      console.log(JSON.stringify(row));
+    });
+  */  
+    
+    client
+    .query("CREATE TABLE IF NOT EXISTS emp(id INT primary key, first_name varchar(64), last_name varchar(64), company_name varchar(64), address varchar(64), city varchar(64), county varchar(64), state varchar(64), zip varchar(8), phone1 varchar(15), email varchar(64))");
+    console.log('table created');
+});
---
+++ insertPostgres.js
@@ -0,0 +1,10 @@
+
+
+
+var pg = require('pg');
+
+pg.connect("postgres://msetubdazzaoeh:tFnxTsFV4abVjFzkBrbFCsGpjX@ec2-107-21-221-59.compute-1.amazonaws.com:5432/d446894mce560p", function(err, client) {
+  if (err) throw err;
+  console.log('Connected to postgres! Getting schemas...');
+
+copy emp(id,first_name,last_name,company_name,address,city,county,state,zip,phone1,email) FROM 'us-500.csv' WITH DELIMITER AS ',' CSV;
