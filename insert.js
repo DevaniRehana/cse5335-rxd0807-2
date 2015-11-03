6f2d23242f1634e69e1cc54f67021fb9aaee3ae0
REHANA DEVANI <Rehana@REHANAs-MacBook-Pro.local>
2015-11-02 19:53:31 -0600

    push code

--- insert.js
+++ insert.js
@@ -10,7 +10,7 @@ var conString = "pg://msetubdazzaoeh:tFnxTsFV4abVjFzkBrbFCsGpjX@ec2-107-21-221-5
 var client = new pg.Client(conString);
 client.connect();
 
-client.query("CREATE TABLE IF NOT EXISTS emps(first_name varchar(64), last_name varchar(64), company_name varchar(64), address varchar(64), city varchar(64), county varchar(64), state varchar(64), zip varchar(8), phone1 varchar(15), email varchar(64), web citext )");
+client.query("CREATE TABLE IF NOT EXISTS emps(id int, first_name varchar(64), last_name varchar(64), company_name varchar(64), address varchar(64), city varchar(64), county varchar(64), state varchar(64), zip varchar(8), phone1 varchar(15), email varchar(64), web citext )");
 
 */
 
@@ -20,10 +20,16 @@ var pg = require('pg');
 pg.connect("postgres://msetubdazzaoeh:tFnxTsFV4abVjFzkBrbFCsGpjX@ec2-107-21-221-59.compute-1.amazonaws.com:5432/d446894mce560p", function(err, client) {
   if (err) throw err;
   console.log('Connected to postgres! Getting schemas...');
-
+/*
   client
     .query('SELECT table_schema,table_name FROM information_schema.tables;')
     .on('row', function(row) {
       console.log(JSON.stringify(row));
     });
+  */  
+    
+    client
+    .query("CREATE TABLE IF NOT EXISTS emp(id INT primary key, first_name varchar(64), last_name varchar(64), company_name varchar(64), address varchar(64), city varchar(64), county varchar(64), state varchar(64), zip varchar(8), phone1 varchar(15), email varchar(64), web citext )");
+    console.log('table created');
 });
+  });
