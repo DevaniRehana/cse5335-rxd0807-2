cdaf29613cfc2b0ce7a8db2dee9028c1bd304370
REHANA DEVANI <Rehana@REHANAs-MacBook-Pro.local>
2015-11-02 20:11:15 -0600

    push code

--- insert.js
+++ insert.js
@@ -20,6 +20,7 @@ var pg = require('pg');
 pg.connect("postgres://msetubdazzaoeh:tFnxTsFV4abVjFzkBrbFCsGpjX@ec2-107-21-221-59.compute-1.amazonaws.com:5432/d446894mce560p", function(err, client) {
   if (err) throw err;
   console.log('Connected to postgres! Getting schemas...');
+  console.log('Creating table...\n');
 /*
   client
     .query('SELECT table_schema,table_name FROM information_schema.tables;')
