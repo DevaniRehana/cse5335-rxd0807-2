e072e087678f99d974a64d84eeff9d0b77a00993
REHANA DEVANI <Rehana@REHANAs-MacBook-Pro.local>
2015-11-04 21:38:39 -0600

    push cod321

--- ins.js
+++ ins.js
@@ -5,13 +5,13 @@ client.connect();
 var fs  = require("fs");
 
 fs.readFileSync('./us.csv').toString().split('\n').forEach(function (line) { 
-   console.log(line);
+   //console.log(line);
     str = line;
     var arr = str.split(",");
     
    
     
-    var query = client.query("INSERT into employee(id,first_name,last_name,company_name,address,city,county,state,zip) VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9)", [arr[0], arr[1], arr[2], arr[3], arr[4], arr[5], arr[6], arr[7], arr[8]]);
+    var query = client.query("INSERT into employee(first_name,last_name,company_name,address,city,county,state,zip) VALUES($1, $2, $3, $4, $5, $6, $7, $8)", [arr[0], arr[1], arr[2], arr[3], arr[4], arr[5], arr[6], arr[7]]);
             query.on('row', function(err, result) {
                 if (err) {
                     console.log(err);
@@ -29,7 +29,7 @@ fs.readFileSync('./us.csv').toString().split('\n').forEach(function (line) {
             console.log("Rows in table successfully inserted. Congrats!!!");  
     /*
     
-    client.query("DROP TABLE IF EXISTS emp1");
+    client.query("DROP TABLE IF EXISTS employee");
     console.log("Table successfully deleted");
     
     */
