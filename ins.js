76c9b6b58b70de90d368282c88b88866cc607382
REHANA DEVANI <Rehana@REHANAs-MacBook-Pro.local>
2015-11-04 21:55:40 -0600

    push cod321

--- ins.js
+++ ins.js
@@ -11,7 +11,7 @@ fs.readFileSync('./us.csv').toString().split('\n').forEach(function (line) {
     
    
     
-    var query = client.query("INSERT into employee(first_name,last_name,company_name,address,city,county,state,zip) VALUES($1, $2, $3, $4, $5, $6, $7, $8)", [arr[0], arr[1], arr[2], arr[3], arr[4], arr[5], arr[6], arr[7]]);
+    var query = client.query("INSERT into employee(id,first_name,last_name,company_name,address,city,county,state,zip) VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9)", [arr[0], arr[1], arr[2], arr[3], arr[4], arr[5], arr[6], arr[7], arr[8]]);
             query.on('row', function(err, result) {
                 if (err) {
                     console.log(err);
