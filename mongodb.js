4a850024db79e116053520338d7fbc7626e1df97
REHANA DEVANI <Rehana@REHANAs-MacBook-Pro.local>
2015-11-05 12:12:32 -0600

    push cod321

--- mongodb.js
+++ mongodb.js
@@ -1,15 +1,13 @@
 
-var connect = require('connect');
-
-
-//lets require/import the mongodb native drivers.
- var mongodb = require('mongodb');
+var con = require('connect');
+var mon = require('mongodb');
  
- mongodb.connect('mongodb://heroku_93gddv1q:i90vj85qr27o8a88106ql2glnj@ds049104.mongolab.com:49104/heroku_93gddv1q', function(err,db){
+ mon.con('mongodb://heroku_93gddv1q:i90vj85qr27o8a88106ql2glnj@ds049104.mongolab.com:49104/heroku_93gddv1q', function(err,db){
+ 
  if (err) {
     console.log('Unable to connect to the mongoDB server. Error:', err);
   } else {
-    console.log('Connection established to');
+    console.log('Connection established. Congrats!!!');
     db.close();
   }
   
