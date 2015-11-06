4ae959cfb420c2c3a0e855255253e064109f5bc1
REHANA DEVANI <Rehana@REHANAs-MacBook-Pro.local>
2015-11-06 12:55:01 -0600

    push cod321

--- mongotry.js
+++ mongotry.js
@@ -4,7 +4,25 @@ var MongoClient = require('mongodb').MongoClient;
 
 var url = "mongodb://heroku_93gddv1q:i90vj85qr27o8a88106ql2glnj@ds049104.mongolab.com:49104/heroku_93gddv1q";
 MongoClient.connect(url,function(err,db){
-	console.log("connected correcctly to server");
-	db.close();
-
+	console.log("connected correctly to server");
+	//db.close();
+   
+   db.createCollection("employees"); 
+   var fs  = require("fs");
+   
+   db.createCollection("employees"); 
+   console.log('Table created!');
+   
+   /*
+   db.open(function(err, client){
+   MongoClient.createCollection("employees", function(err, col) {
+   MongoClient.collection("employees", function(err, col) {
+             for (var i = 0; i < 100; i++) {
+                 col.insert({c:i}, function() {});
+             }
+         });
+    });
+});
+   
+   */
 });
