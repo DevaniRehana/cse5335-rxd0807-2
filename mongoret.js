1ceed40e54c1a3a47784b97244f90ecc16f0b53f
REHANA DEVANI <Rehana@REHANAs-MacBook-Pro.local>
2015-11-06 14:24:07 -0600

    push cod321

--- mongoret.js
+++ mongoret.js
@@ -1,6 +1,7 @@
 
 
 var MongoClient = require('mongodb').MongoClient;
+var assert = require('assert');
 
 var url = "mongodb://heroku_93gddv1q:i90vj85qr27o8a88106ql2glnj@ds049104.mongolab.com:49104/heroku_93gddv1q";
 MongoClient.connect(url,function(err,db){
