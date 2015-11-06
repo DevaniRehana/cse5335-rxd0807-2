caeef9c60b3df53c53b5b57431ca510908b8f5d8
REHANA DEVANI <Rehana@REHANAs-MacBook-Pro.local>
2015-11-06 14:15:29 -0600

    push cod321

--- mongoret.js
+++ mongoret.js
@@ -8,7 +8,7 @@ MongoClient.connect(url,function(err,db){
 	
 
 var getById = function(db, callback) {
-   var cursor =db.collection('employees').find("id":1 );
+   var cursor =db.collection('employees').find("first_name":"James" );
    cursor.each(function(err, doc) {
       assert.equal(err, null);
       if (doc != null) {
