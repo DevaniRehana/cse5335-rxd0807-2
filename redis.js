b4dcd1ca8a15c63a109a5dd0b00cc5c5ca870da9
REHANA DEVANI <Rehana@REHANAs-MacBook-Pro.local>
2015-11-06 18:31:29 -0600

    push cod321

--- redis.js
+++ redis.js
@@ -1,6 +1,6 @@
 
 var redis = require('redis');
-var client = redis.createClient(6499, 'ec2-54-83-59-218.compute-1.amazonaws.com);
+var client = redis.createClient(6499, 'ec2-54-83-59-218.compute-1.amazonaws.com');
 client.auth('p8naufj27ovrb0c31g4iru1gk18', function (err) {
     if (err) then throw err;
 });
