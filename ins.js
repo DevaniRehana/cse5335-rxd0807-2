9f591c2c7d27261d429a520d81582bf8c5dd4c8e
REHANA DEVANI <Rehana@REHANAs-MacBook-Pro.local>
2015-11-09 13:43:57 -0600

    push code

--- ins.js
+++ ins.js
@@ -13,6 +13,8 @@ fs.readFileSync('./us.csv').toString().split('\n').forEach(function (line) {
    
     //var in = "INSERT into employee(id,first_name,last_name,company_name,address,city,county,state,zip) VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9)", [arr[0], arr[1], arr[2], arr[3], arr[4], arr[5], arr[6], arr[7], arr[8]]
     var query = client.query("INSERT into employee(id,first_name,last_name,company_name,address,city,county,state,zip) VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9)", [id1, arr[1], arr[2], arr[3], arr[4], arr[5], arr[6], arr[7], arr[8]]);
+    console.log("Rows in table successfully inserted. Congrats!!!");  
+
             query.on('row', function(err, result) {
                 if (err) {
                     console.log(err);
@@ -27,7 +29,6 @@ fs.readFileSync('./us.csv').toString().split('\n').forEach(function (line) {
                     client.end();
                 }
             });    
-           // console.log("Rows in table successfully inserted. Congrats!!!");  
     /*
     
     client.query("DROP TABLE IF EXISTS employee");
--- mongoinstry.js
+++ mongoinstry.js
@@ -1,5 +1,7 @@
 
 
+
+
 var MongoClient = require('mongodb').MongoClient;
 
 var url = "mongodb://heroku_93gddv1q:i90vj85qr27o8a88106ql2glnj@ds049104.mongolab.com:49104/heroku_93gddv1q";
@@ -21,7 +23,25 @@ MongoClient.connect(url,function(err,db){
 		}
 		else
 		{
-			var arr = JSON.parse(data);
+			
+			var doc = {
+			_id: employees.id,
+			first_name: employees.first_name,
+			last_name: employees.last_name,
+			company_name: employees.company_name,
+			address: employees.address,
+			city: employees.city,
+			country: employees.country,
+			state: employees.state,
+			zip : employees,zip
+			
+			}
+			
+			var arr = JSON.parse(doc);
+
+			
+			
+			
 			
 		    for (var i = 0; i < arr.length; i++) {
       	    db.collection('employees').insertOne(arr[i], function(err,result){
