13c748928c633bf9dbea35f0b96c6af31b3f469a
REHANA DEVANI <Rehana@REHANAs-MacBook-Pro.local>
2015-11-09 14:02:03 -0600

    push code

--- mongoins.js
+++ mongoins.js
@@ -6,9 +6,6 @@ var url = "mongodb://heroku_93gddv1q:i90vj85qr27o8a88106ql2glnj@ds049104.mongola
 MongoClient.connect(url,function(err,db){
 	console.log("connected correctly to server");
 	//db.close();
-   
-  
-   
    db.createCollection("employees"); 
    console.log('Table created!');
    
@@ -25,17 +22,16 @@ MongoClient.connect(url,function(err,db){
 			
 		    for (var i = 0; i < arr.length; i++) {
       	    db.collection('employees').insertOne(arr[i], function(err,result){
-       		 if(err)
-        {
-        console.log('err');
-        }
-        else{
-        console.log(result);
-        console.log("Successfully inserted");
-		}
-		});
-		}
-		}
+       		 		if(err){
+        				console.log('err');
+        			}
+        			else{
+        				console.log(result);
+        				console.log("Successfully inserted");
+					}
+			});
+			}
+		}	
 	});	
    
 });
--- mongoinstry.js
+++ mongoinstry.js
@@ -1,15 +1,10 @@
 
-
-
-
 var MongoClient = require('mongodb').MongoClient;
 
 var url = "mongodb://heroku_93gddv1q:i90vj85qr27o8a88106ql2glnj@ds049104.mongolab.com:49104/heroku_93gddv1q";
 MongoClient.connect(url,function(err,db){
 	console.log("connected correctly to server");
 	//db.close();
-   
-  
    
    db.createCollection("employees"); 
    console.log('Table created!');
@@ -39,10 +34,6 @@ MongoClient.connect(url,function(err,db){
 			
 			var arr = JSON.parse(doc);
 
-			
-			
-			
-			
 		    for (var i = 0; i < arr.length; i++) {
       	    db.collection('employees').insertOne(arr[i], function(err,result){
        		 if(err)
