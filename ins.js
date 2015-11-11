80d6b7e0a4f8a7bb0e5a5c6905169a3f7236552f
REHANA DEVANI <Rehana@REHANAs-MacBook-Pro.local>
2015-11-11 12:37:59 -0600

    push code

--- ins.js
+++ ins.js
@@ -10,13 +10,15 @@ fs.readFileSync('./us.csv').toString().split('\n').forEach(function (line) {
     var arr = str.split(",");
     console.log(arr);
     var id1 = parseInt(arr[0]);
+    
+    /*
    
     var query = client.query("INSERT into employee(id,first_name,last_name,company_name,address,city,county,state,zip) VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9)", [id1, arr[1], arr[2], arr[3], arr[4], arr[5], arr[6], arr[7], arr[8]]);
     console.log("Rows in table successfully inserted. Congrats!!!");  
 
      
 
-/*
+
 
 
             query.on('row', function(err, result) {
@@ -33,8 +35,8 @@ fs.readFileSync('./us.csv').toString().split('\n').forEach(function (line) {
                     client.end();
                 }
             });    
-    */
     
+    */
     client.query("DROP TABLE IF EXISTS employee");
     console.log("Table successfully deleted");
     
