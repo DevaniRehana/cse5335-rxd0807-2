76d6dabfe8d4dc9498fd46be7d65731c25a7e841
REHANA DEVANI <Rehana@REHANAs-MacBook-Pro.local>
2015-11-11 13:21:11 -0600

    push code

--- mongoret.js
+++ mongoret.js
@@ -9,7 +9,7 @@ MongoClient.connect(url,function(err,db){
 	
 
 var getByName = function(db, callback) {
-   var cursor =db.collection('employees').find({"country":"New York"});
+   var cursor =db.collection('employees').find({"city":"New York"});
    cursor.each(function(err, doc) {
       assert.equal(err, null);
       if (doc != null) {
