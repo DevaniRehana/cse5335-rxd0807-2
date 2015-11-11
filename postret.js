b7cf4b9a22511aa7706862599aacab43f020849a
REHANA DEVANI <Rehana@REHANAs-MacBook-Pro.local>
2015-11-11 12:21:57 -0600

    push code

---
+++ postret.js
@@ -0,0 +1,23 @@
+
+
+
+var pg = require('pg');
+var client = new pg.Client("postgres://msetubdazzaoeh:tFnxTsFV4abVjFzkBrbFCsGpjX@ec2-107-21-221-59.compute-1.amazonaws.com:5432/d446894mce560p");
+client.connect();
+var fs  = require("fs");
+
+
+   
+    var query = client.query("SELECT * FROM employee where id = 1", function(err, results){
+   	 if (err){
+   	    throw err;
+   	 }
+   	 response.send(results.rows);
+
+     
+   });
+          
+    
+    
+    
+
