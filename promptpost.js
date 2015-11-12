759ddb0ff421bd573b116b844aec5841bf1a265f
REHANA DEVANI <Rehana@REHANAs-MacBook-Pro.local>
2015-11-11 18:58:42 -0600

    push code

--- promptpost.js
+++ promptpost.js
@@ -3,42 +3,23 @@ var pg = require('pg');
 var client = new pg.Client("postgres://msetubdazzaoeh:tFnxTsFV4abVjFzkBrbFCsGpjX@ec2-107-21-221-59.compute-1.amazonaws.com:5432/d446894mce560p");
 client.connect();
 var fs  = require("fs");
+var prompt = require('prompt');
 
-fs.readFileSync('./us.csv').toString().split('\n').forEach(function (line) { 
-   console.log(line);
-    str = line;
-    var arr = str.split(",");
-    console.log(arr);
-    var id1 = parseInt(arr[0]);
+prompt.start();
+
+
+    
+    
    
-    var query = client.query("INSERT into employee(id,first_name,last_name,company_name,address,city,county,state,zip) VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9)", [id1, arr[1], arr[2], arr[3], arr[4], arr[5], arr[6], arr[7], arr[8]]);
-    console.log("Rows in table successfully inserted. Congrats!!!");  
-
-     
-
-
-
-
-            query.on('row', function(err, result) {
-                if (err) {
-                    console.log(err);
-                } else {
-                    console.log('row inserted with id: ' + result.rows[0].id);
-                }
-
-                count++;
-                console.log('count = ' + count);
-                if (count == 100) {
-                    console.log('Client will end now!!!');
-                    client.end();
-                }
-            });    
-    /*
+     prompt.get(['id', 'city'], function (err, result) {
     
-    client.query("DROP TABLE IF EXISTS employee");
-    console.log("Table successfully deleted");
+    console.log('Command-line input received:');
+    console.log('  id: ' + result.id);
+    console.log('  city: ' + result.city);
+  });
+
+
+
+
     
-    */
-    
-    
-});
+
