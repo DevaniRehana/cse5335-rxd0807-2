aae0e38d7d39819c7129d7a752f51759c821ca9b
REHANA DEVANI <Rehana@REHANAs-MacBook-Pro.local>
2015-11-14 15:34:54 -0600

    push code

--- redisnew.js
+++ redisnew.js
@@ -4,3 +4,9 @@
 var redis = require('redis');
 var client = redis.createClient(18199, 'ec2-54-83-9-36.compute-1.amazonaws.com');
 client.auth('p7ue7eg7cab4re7m4qejdl08p3s');
+
+
+client.on('connect', function() {
+console.log('Connected to Redis');
+
+});
