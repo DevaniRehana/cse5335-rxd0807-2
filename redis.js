f099d5ae8dedfd48395e19d30c242fe6d0cd2f63
REHANA DEVANI <Rehana@REHANAs-MacBook-Pro.local>
2015-11-14 00:31:50 -0600

    push code

--- redis.js
+++ redis.js
@@ -3,7 +3,7 @@ var redis = require('redis');
 var client = redis.createClient(6499, 'ec2-54-83-59-218.compute-1.amazonaws.com');
 client.auth('p8naufj27ovrb0c31g4iru1gk18');
 
-
+/*
 
 try
     console.log("clean sessions");
@@ -12,7 +12,7 @@ try
     console.log("error:", err);
 
 
-
+*/
 
 
 client.on('connect', function() {
