222e9b7857729ac8f0eaa895297f74151dd527dd
REHANA DEVANI <Rehana@REHANAs-MacBook-Pro.local>
2015-11-14 17:18:45 -0600

    push code

--- redisretpk.js
+++ redisretpk.js
@@ -7,7 +7,7 @@ var client = redis.createClient(18199, 'ec2-54-83-9-36.compute-1.amazonaws.com')
 
 client.auth('p7ue7eg7cab4re7m4qejdl08p3s');
 
-redis_scanner.bindScanners(client);
+//redis_scanner.bindScanners(client);
 
 
 client.on('connect', function() {
