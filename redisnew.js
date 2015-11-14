a35408c255a8a406158226ecc623a9694afeea9b
REHANA DEVANI <Rehana@REHANAs-MacBook-Pro.local>
2015-11-14 15:38:54 -0600

    push code

--- redisnew.js
+++ redisnew.js
@@ -9,4 +9,47 @@ client.auth('p7ue7eg7cab4re7m4qejdl08p3s');
 client.on('connect', function() {
 console.log('Connected to Redis');
 
+
+
+
+   var fs  = require("fs");
+   fs.readFile('./us.json', 'utf8', function(err,data){
+
+		if(err)
+		{
+			console.log('err');
+		}
+		
+		else {
+		
+
+			var arrObj = JSON.parse(data);
+			
+		    for (var i = 0; i < arrObj.length; i++) {
+		    		    
+	
+	
+	var obj = arrObj[i];
+	/*
+	client.rpush([''+obj.id, obj.first_name,obj.last_name,obj.company_name,obj.address,obj.city,obj.county,obj.state,""+obj.zip], function(err, reply) {
+    console.log("reply "+reply); //prints 2
+    console.log(err);
+});
+	*/
+	
+	
+	client.hmset(''+obj.id, obj, function(err, object) {
+    console.log(object);
+});
+	
+	}
+	
+	
+	
+	client.quit(function (err, res) {
+    console.log('Exiting from quit command.');
+});
+
+}
+});
 });
