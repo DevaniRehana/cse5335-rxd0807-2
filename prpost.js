6e52d879f2b5d04d4e81cf698574effaef8e3dba
REHANA DEVANI <Rehana@REHANAs-MacBook-Pro.local>
2015-11-15 16:00:15 -0600

    push code

---
+++ prpost.js
@@ -0,0 +1,22 @@
+
+var pg = require('pg');
+var client = new pg.Client("postgres://msetubdazzaoeh:tFnxTsFV4abVjFzkBrbFCsGpjX@ec2-107-21-221-59.compute-1.amazonaws.com:5432/d446894mce560p");
+client.connect();
+var fs  = require("fs");
+
+
+var readlineSync = require('readline-sync');
+ 
+// Wait for user's response. 
+var pid = readlineSync.question('May I have your ID? :');
+
+
+
+    var query = client.query("SELECT * FROM employee where county id = pid", function(err, results){
+   	 if (err){
+   	    throw err;
+   	 }
+   	 console.log(results.rows);
+
+     
+   });
