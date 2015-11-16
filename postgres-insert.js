53142dcf1a086360c07850fecb2cc2de0e8808c0
REHANA DEVANI <Rehana@REHANAs-MacBook-Pro.local>
2015-11-16 16:04:48 -0600

    push code

--- postgres-insert.js
+++ postgres-insert.js
@@ -11,8 +11,7 @@ fs.readFileSync('./us.csv').toString().split('\n').forEach(function (line) {
     console.log(arr);
     var id1 = parseInt(arr[0]);
     
-    //comment from here to drop the database
-   
+   /*
     var query = client.query("INSERT into employee(id,first_name,last_name,company_name,address,city,county,state,zip) VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9)", [id1, arr[1], arr[2], arr[3], arr[4], arr[5], arr[6], arr[7], arr[8]]);
     console.log("Rows in table successfully inserted. Congrats!!!");  
 
@@ -36,14 +35,13 @@ fs.readFileSync('./us.csv').toString().split('\n').forEach(function (line) {
                 }
             });   
             
-        //comment till here to drop the database first    
-            
-           /*
+*/            
+        
     
     client.query("DROP TABLE IF EXISTS employee");
     console.log("Table successfully deleted");
     
 
-    */
+  
     
 });
